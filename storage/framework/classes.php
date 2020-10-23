a61c856393bb585b69afe1e2f4ddbc1f9e65d50c
sergei kuznetsov <segakuz@gmail.com>
2020-10-23 12:16:31 +0500

    init project

---
+++ storage/framework/classes.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'october\\demo\\Plugin' => 'plugins\\october\\demo\\Plugin.php',
+  'backend\\Controllers\\index' => 'modules\\backend\\controllers\\index.php',
+  'backend\\Controllers\\auth' => 'modules\\backend\\controllers\\auth.php',
+  'system\\Controllers\\requestlogs' => 'modules\\system\\controllers\\requestlogs.php',
+  'system\\Controllers\\settings' => 'modules\\system\\controllers\\settings.php',
+);
